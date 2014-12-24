76d530b6b0e778128fc9836ebb58b8a73ebbe5fd
gs-nyarlagadda <nyarlagadda@gainsight.com>
2014-12-24 13:22:35 +0530

    My First Commit

---
+++ src/Testsample/Add.java
@@ -0,0 +1,14 @@
+package Testsample;
+
+public class Add {
+	
+	
+	public int add(int x, int y){
+		
+		int c;
+		c= x+y;
+		return c;
+		
+	}
+
+}
---
+++ src/Testsample/Addition.java
@@ -0,0 +1,15 @@
+package Testsample;
+
+public class Addition {
+	
+	public static void main(String[] args){
+		
+	Add a =  new Add();
+	int sum = a.add(2, 19);
+	
+	
+	System.out.println("RESULT ="+ sum);
+	
+	}
+
+}
