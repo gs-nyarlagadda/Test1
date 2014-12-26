1b86700d8827f696488fa7af157ce1e08995c23f
gs-nyarlagadda <nyarlagadda@gainsight.com>
2014-12-26 15:23:27 +0530

    Done

---
+++ src/Testsample/Params.java
@@ -0,0 +1,17 @@
+package Testsample;
+
+public class Params {
+	
+		public static void time(int x,int y){
+			
+			System.out.println(x*y);
+		}
+
+	public static void main(String[] args){
+		
+		time(2,2);// passing parameters 
+		
+	}
+	
+}
+
