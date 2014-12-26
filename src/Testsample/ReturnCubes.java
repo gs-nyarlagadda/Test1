af13659f15569f5437c31aa97f93771147757a88
gs-nyarlagadda <nyarlagadda@gainsight.com>
2014-12-26 15:33:05 +0530

    To Return Values

---
+++ src/Testsample/ReturnCubes.java
@@ -0,0 +1,15 @@
+package Testsample;
+
+public class ReturnCubes {
+	
+	public static int cube(int x){
+		
+		return x*x*x;
+	}
+	
+	public static void main(String[] args){
+	
+	System.out.println(cube(3));
+	System.out.println(cube(5));
+  }
+}
