ef5f10989e04dacd9e6ab55c98854d9279d97be3
gs-nyarlagadda <nyarlagadda@gainsight.com>
2014-12-26 14:17:16 +0530

    Comitted

---
+++ src/Testsample/PasswordGen.java
@@ -0,0 +1,15 @@
+package Testsample;
+
+public class PasswordGen {
+	
+	public static void main(String[] args){
+		
+		String username = "Nithya";
+		String role = "SFDC";
+		String DOB = "15";
+		int age = 24;
+		
+		System.out.println("Password :" + username + age + role +DOB);// String Concatenation
+	}
+
+}
