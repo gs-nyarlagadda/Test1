e80b2ad5be043d29efc773f3f9d560347349c492
gs-nyarlagadda <nyarlagadda@gainsight.com>
2014-12-26 13:54:39 +0530

    Successfully Committed

---
+++ src/Testsample/Exmp1.java
@@ -0,0 +1,13 @@
+package Testsample;
+
+public class Exmp1 {
+	
+	public static void main(String[] args){
+		
+		String ln = "Java";
+		System.out.println(ln);
+		ln = "Core Java";
+		System.out.println(ln);
+	}
+
+}
