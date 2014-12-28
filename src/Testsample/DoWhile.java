180df8456d71f51266e131622377400f4e7a3fcc
gs-nyarlagadda <nyarlagadda@gainsight.com>
2014-12-28 18:55:21 +0530

    Do-While Exp

---
+++ src/Testsample/DoWhile.java
@@ -0,0 +1,14 @@
+package Testsample;
+
+public class DoWhile {
+
+	public static void main(String[] args){
+		
+		int x = 1; // declare and initialize the variable
+		do{
+			
+			System.out.println(x);
+			x++;
+		}while(x<=10);
+	}
+}
