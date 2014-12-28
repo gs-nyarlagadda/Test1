6cb795c1e348ac21edc4d3a136598fffeee2868d
gs-nyarlagadda <nyarlagadda@gainsight.com>
2014-12-28 19:14:27 +0530

    Multiple Inintializations in for

---
+++ src/Testsample/MultiFor.java
@@ -0,0 +1,12 @@
+package Testsample;
+
+public class MultiFor {
+
+	public static void main(String[] args){
+		
+		for(int i=1, j=11; i<=11; i++,j--){ // j values will automatically change from 11 to 1
+			
+			System.out.println(i +"\t" +j);
+		}
+	}
+}
